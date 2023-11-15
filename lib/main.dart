0b774424b3ae611387bf2817237013187752d4fc
herdofpane <121870569+herdofpane@users.noreply.github.com>
2023-11-16 00:58:55 +0100

    maj pofinage hitbox

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -32,11 +34,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Random random = Random();
+
   double _imageYPosition = 5.0;
-  double _imageYPosition2 = 50;
-  double _imageXPosition2 = 50;
+  double _imageXPosition = 250;
+
+  double _imageYPosition2 = 200;
+  double _imageXPosition2 = 1000;
+
+  int condition = 255;
   late Timer _timer;
-  final double _gravity = -5.0; // Gravité négative pour aller vers le haut
+  final double _gravity = -5.0;
+  final double _gravity2 = -5.0; // Gravité négative pour aller vers le haut
   final double _jumpStrength = 50.0; // Force de saut positive
   final int _refreshRate = 50;
 
@@ -46,11 +55,24 @@ class _MyHomePageState extends State<MyHomePage> {
     _timer = Timer.periodic(Duration(milliseconds: _refreshRate), (timer) {
       setState(() {
         _imageYPosition += _gravity;
+        _imageXPosition2 += _gravity2;
         if (_imageYPosition > MediaQuery.of(context).size.height) {
           _imageYPosition = MediaQuery.of(context).size.height;
         }
-        if (_imageYPosition == 0) {
+        if (_imageYPosition == 0 ||
+            _imageYPosition < _imageYPosition2 &&
+                _imageXPosition2 < condition &&
+                _imageXPosition2 > 240) {
           _imageYPosition = 5;
+          _imageXPosition2 = 1000;
+        }
+        if (_imageXPosition2 == 0) {
+          _imageXPosition2 = 1000;
+          double nbr = 0;
+          // while (nbr < 250) {
+          nbr = random.nextDouble() * 400;
+          // }
+          _imageYPosition2 = nbr;
         }
       });
     });
@@ -94,6 +116,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Positioned(
               bottom: _imageYPosition,
+              left: _imageXPosition,
               child:
                   Image.asset('image/carlito.jpg'), // Remplacez par votre image
             ),
